38ff9b3560fbece0c1d7a447a983f3849a0f8f56
kemosabe1900 <mati319@revature.net>
2024-05-07 19:58:59 -0500

    app file

--- backend/p2Backend/src/main/java/com/revature/models/Item.java
+++ backend/p2Backend/src/main/java/com/revature/models/Item.java
@@ -1,2 +1,26 @@
-package com.revature.models;public class Item {
+package com.revature.models;
+
+
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name="items")
+public class Item {
+
+    @Id
+    @GeneratedValue
+    private int itemId;
+
+    @Column
+    private String name;
+
+    @Column
+    private double producer_id;
+
+    @Column (nullable = false)
+    private double rating;
+
+
 }
---
+++ backend/p2Backend/src/main/java/com/revature/p2Backend/P2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.p2Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2BackendApplication.class, args);
+	}
+
+}
