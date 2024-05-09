9189b0c5254bf42beaeddc1e727d8f4b3953a12e
William Nettles <willnettles@gmail.com>
2024-05-09 16:08:58 -0400

    Began implementation for view collection

--- backend/p2Backend/src/main/java/com/revature/controllers/CollectionController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/CollectionController.java
@@ -1,10 +1,17 @@
 package com.revature.controllers;
 
+import com.revature.models.Collection;
 import com.revature.services.CollectionService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/collections")
@@ -17,4 +24,16 @@ public class CollectionController {
     public CollectionController(CollectionService collectionService) {
         this.collectionService = collectionService;
     }
+
+
+    @GetMapping("/my_collection")
+    public ResponseEntity<List<Collection>> getCollection(HttpSession session) {
+
+        int userId = 1;
+        //int userId = (int) session.getAttribute(("userId"));
+
+        //if the user is logged in, call userService.getCollection(), otherwise, return an empty list
+        return userId == 0? ResponseEntity.ok(new ArrayList<Collection>()) : ResponseEntity.ok(collectionService.getCollection(userId));
+    }
+
 }
--- backend/p2Backend/src/main/java/com/revature/services/CollectionService.java
+++ backend/p2Backend/src/main/java/com/revature/services/CollectionService.java
@@ -1,7 +1,11 @@
 package com.revature.services;
 
 import com.revature.daos.CollectionDAO;
+import com.revature.models.Collection;
+import com.revature.models.CollectionKey;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CollectionService {
@@ -12,4 +16,12 @@ public class CollectionService {
         this.collectionDAO = collectionDAO;
     }
 
+
+    public List<Collection> getCollection(int userId) {
+        return collectionDAO.findAllByIdUserId(userId);
+
+
+        //having trouble getting the custom DAO method to work so this is a quasi-temporary soltuion
+        //return  collectionDAO.findAll().stream().filter(collection -> {return collection.getId().getUser().getId() == userId;}).toList();
+    }
 }
