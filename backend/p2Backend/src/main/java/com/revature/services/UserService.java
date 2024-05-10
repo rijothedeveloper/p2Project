1e6721418d73938e2dff83b42db61edc4cdb917f
kemosabe1900 <mati319@revature.net>
2024-05-10 12:24:50 -0500

    Implemented findUserByUsername method

--- backend/p2Backend/src/main/java/com/revature/services/UserService.java
+++ backend/p2Backend/src/main/java/com/revature/services/UserService.java
@@ -2,9 +2,11 @@ package com.revature.services;
 
 import com.revature.daos.UserDAO;
 import com.revature.models.User;
+import com.revature.models.dtos.OutgoingUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
@@ -17,7 +19,25 @@ public class UserService {
         this.userDAO = userDAO;
     }
 
-    public Optional<User> findUserByUsername(String username){
-        return userDAO.findByUsername(username);
+    public Optional<OutgoingUserDTO> findUserByUsername(String username){
+
+        Optional<User> user = userDAO.findByUsername(username);
+
+        if(user.isPresent()){
+            User u = user.get();
+            OutgoingUserDTO uout = new OutgoingUserDTO(
+                    u.getId(),
+                    u.getFirstName(),
+                    u.getLastName(),
+                    u.getUsername(),
+                    u.getRole(),
+                    u.getEmail(),
+                    u.getTimestamp()
+            );
+            return Optional.of(uout);
+        } else {
+            return Optional.empty();
+        }
+//        return userDAO.findByUsername(username);
     }
 }
