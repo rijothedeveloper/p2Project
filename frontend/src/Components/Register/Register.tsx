2d6c176ca30d610300a378d1f67dc2b382af5d94
Neil C <74469287+sbacky@users.noreply.github.com>
2024-05-10 13:19:14 -0700

    Base Container Structure (#11)
    * Provide base container structure for any page that uses the navbar
    
    * Provide a base container structure for login and register pages

--- frontend/src/Components/Register/Register.tsx
+++ frontend/src/Components/Register/Register.tsx
@@ -1,8 +1,12 @@
+import { Container } from "react-bootstrap"
+
 export const Register: React.FC = () => {
 
     return (
-        <div>
-            
-        </div>
+        <Container className="d-flex flex-column justify-content-center m-5 px-5">
+            <Container className="w-75 p-3">
+
+            </Container>
+        </Container>
     )
 }
