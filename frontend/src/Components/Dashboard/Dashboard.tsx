d19b95387c28e5add8df5c3f2176340dbc4b5ed4
Neil Crum <nhc.crum@outlook.com>
2024-05-10 14:57:42 -0700

    Add skeleton for dashboard

--- frontend/src/Components/Dashboard/Dashboard.tsx
+++ frontend/src/Components/Dashboard/Dashboard.tsx
@@ -1,10 +1,68 @@
+import { useContext, useState } from "react";
+import { Container, Form } from "react-bootstrap"
+import { UserContext } from "../../Contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+
 export const Dashboard: React.FC = () => {
 
     // TODO: Create the actual dashboard. This is skeleton code for the dashboard component. It should display the current users collection of items, reviews and maybe their information.
 
-    return (
-        <div>
+    // Navigation to navigate to different pages
+    const navigate = useNavigate();
+    // Get currentUser from user context
+    const { currentUser } = useContext(UserContext);
+    // Check if a user is logged in
+    if (!currentUser) {
+        navigate("/");
+    }
 
-        </div>
+    // Current view state - defaults to myCollection
+    const [currentView, setCurrentView] = useState("myCollection");
+    // My collection state - defaults to an empty array
+    const [myCollection, setMyCollection] = useState([]);
+    // My reviews state - defaults to an empty array
+    const [myReviews, setMyReviews] = useState([]);
+
+    /**
+     * Handles the select view on change to change the current view in the dashboard
+     * @param input - select input value
+     */
+    const handleSelectViewOnChange = (input: React.ChangeEvent<HTMLSelectElement>) => {
+        const view: string = input.target.value;
+        switch (view) {
+            case "myCollection":
+                // TODO: add function to get collection items for logged in users
+                setCurrentView("myCollection");
+                break;
+            case "myReviews":
+                // TODO: add function to get review items for logged in users
+                setCurrentView("myReviews");
+                break;
+            default:
+                // This should never run, but just in case
+                alert("Invalid view type!");
+                break;
+        }
+    };
+
+    return (
+        <>
+            <Container id="profile-container">
+                {/* Can add logged in user information here */}
+            </Container>
+            <Container id="select-view-container">
+                <Form id="select-view-form">
+                    <Form.FloatingLabel label="Select View" controlId="dashboard-select-view" onChange={handleSelectViewOnChange}>
+                        <Form.Select id="selectMyView" defaultValue="myCollection">
+                            <option value="myCollection">My Collection</option>
+                            <option value="myReviews">My Reviews</option>
+                        </Form.Select>
+                    </Form.FloatingLabel>
+                </Form>
+            </Container>
+            <Container id="collection-review-container">
+                {/* Conditionally render my collection or my reviews based on currentView */}
+            </Container>
+        </>
     )
 }
