daee279aeb21738dbe329ae62a2093d0bc14f0a3
Mati Nemera <62734465+Kemosabe1900@users.noreply.github.com>
2024-05-10 11:09:16 -0500

    find user by username (#7)
    Co-authored-by: kemosabe1900 <mati319@revature.net>

--- backend/p2Backend/src/main/java/com/revature/controllers/UserController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/UserController.java
@@ -2,12 +2,16 @@ package com.revature.controllers;
 
 
 import com.revature.daos.UserDAO;
+import com.revature.models.User;
+import com.revature.models.dtos.OutgoingUserDTO;
 import com.revature.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -19,6 +23,18 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<?> findUserByUsername(@PathVariable String username) {
+        Optional<User> user = userService.findUserByUsername(username);
+
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Username does not exist");
+        }
 
     }
 }
