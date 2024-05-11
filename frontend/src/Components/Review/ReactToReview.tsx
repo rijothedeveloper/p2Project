0b612be3fdc07ae8aaa2cc576dbe6464e5cc2fb4
Neil Crum <nhc.crum@outlook.com>
2024-05-10 18:19:42 -0700

    Add skeleton for ReactToReview component

---
+++ frontend/src/Components/Review/ReactToReview.tsx
@@ -0,0 +1,10 @@
+import { BsHandThumbsDown, BsHandThumbsDownFill, BsHandThumbsUp, BsHandThumbsUpFill } from "react-icons/bs";
+
+export const ReactToReview: React.FC = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
