4a1ac9f98a646ca120c85197f9e3bbfe407034ae
William Nettles <willnettles@gmail.com>
2024-05-11 15:42:57 -0400

    Added follow user and unfollow user functionality

--- backend/p2Backend/src/main/java/com/revature/controllers/FollowController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/FollowController.java
@@ -1,10 +1,11 @@
 package com.revature.controllers;
 
 import com.revature.services.FollowService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/follows")
@@ -16,5 +17,35 @@ public class FollowController {
     @Autowired
     public FollowController(FollowService followService) {
         this.followService = followService;
+    }
+
+    @PostMapping("/new_follow/{targetUserId}")
+    public ResponseEntity<Object> followUser(@PathVariable int targetUserId, HttpSession session) {
+
+        if ((int) session.getAttribute("userId") == 0) {
+            return ResponseEntity.status(401).body("You must be logged in to follow a user.");
+        }
+
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(followService.followUser(targetUserId, (int) session.getAttribute("userId")));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/unfollow/{targetUserId}")
+    public ResponseEntity<Object> unfollowUser(@PathVariable int targetUserId, HttpSession session) {
+
+        if ((int) session.getAttribute("userId") == 0) {
+            return ResponseEntity.status(401).body("You must be logged in to unfollow a user.");
+        }
+
+        try {
+            followService.unfollowUser(targetUserId, (int) session.getAttribute("userId"));
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
     }
 }
--- backend/p2Backend/src/main/java/com/revature/services/FollowService.java
+++ backend/p2Backend/src/main/java/com/revature/services/FollowService.java
@@ -1,16 +1,50 @@
 package com.revature.services;
 
 import com.revature.daos.FollowDAO;
+import com.revature.daos.UserDAO;
+import com.revature.models.Follow;
+import com.revature.models.FollowKey;
+import com.revature.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FollowService {
 
     FollowDAO followDAO;
+    UserDAO userDAO;
 
     @Autowired
-    public FollowService(FollowDAO followDAO) {
+    public FollowService(FollowDAO followDAO, UserDAO userDAO) {
+        this.userDAO = userDAO;
         this.followDAO = followDAO;
+    }
+
+    public Follow followUser(int targetUserId, int userId) {
+
+        Optional<User> user = userDAO.findById(userId);
+        Optional<User> targetUser = userDAO.findById(targetUserId);
+
+        if (user.isEmpty() || targetUser.isEmpty()) {
+            throw new RuntimeException("User not found.");
+        }
+        FollowKey key = new FollowKey(user.get(), targetUser.get());
+        Follow follow = new Follow(key);
+        return followDAO.save(follow);
+    }
+
+    public void unfollowUser(int targetUserId, int userId) {
+
+            Optional<User> user = userDAO.findById(userId);
+            Optional<User> targetUser = userDAO.findById(targetUserId);
+
+            if (user.isEmpty() || targetUser.isEmpty()) {
+                throw new RuntimeException("User not found.");
+            }
+            FollowKey key = new FollowKey(user.get(), targetUser.get());
+            followDAO.deleteById(key);
     }
 }
