064106d708d34e374112ad2d4ae19b445f2f50d5
JemalSadik <139152851+JemalSadik@users.noreply.github.com>
2024-05-11 14:47:42 -0500

    Delete item (#13)
    * Fixed bugs in the skeleton
    
    * Fixed bugs in the skeleton pt 2
    
    * Fixed bugs in the skeleton pt 3
    
    * Fixed bugs in the skeleton
    
    * Fixed bugs in the skeleton pt 2
    
    * Added methods getAllItems,
                  getItemById,
                  getItemByName,
                  updateItem,
                  and deleteItem
    
    * Added methods getAllItems,
                  getItemById,
                  getItemByName,
                  updateItem,
                  and deleteItem
    
    * @CrossOrigin - I just keep it as the original
    
    ---------
    
    Co-authored-by: William Nettles <willnettles@gmail.com>
    Co-authored-by: William Nettles <65319041+William-Nettles@users.noreply.github.com>

--- backend/p2Backend/src/main/java/com/revature/daos/ItemDAO.java
+++ backend/p2Backend/src/main/java/com/revature/daos/ItemDAO.java
@@ -4,6 +4,10 @@ import com.revature.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ItemDAO extends JpaRepository<Item,Integer> {
+
+    public Optional<Item> findByName(String name);
 }
