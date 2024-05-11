7976ca402acaaad45af7ee2a5fcbf286585cf1f5
Neil Crum <nhc.crum@outlook.com>
2024-05-10 20:51:56 -0700

    Reflect the changes in BaseLayout branch

--- frontend/src/Components/Layout/Layout.tsx
+++ frontend/src/Components/Layout/Layout.tsx
@@ -1,14 +1,15 @@
 import { Container } from "react-bootstrap"
 import { NavbarComp } from "../NavbarComp/NavbarComp"
+import { Outlet } from "react-router-dom"
 
-export const Layout: React.FC<any> = ({children}) => {
+export const Layout: React.FC<any> = () => {
 
     return (
         <>
             <NavbarComp />
             <Container className="d-flex flex-column justify-content-center m-5 px-5">
                 <Container className="w-75 p-3">
-                    {children}
+                    <Outlet />
                 </Container>
             </Container>
         </>
