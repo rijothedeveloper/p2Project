b0d543b934ee6cc01654ee6369d8510e0a0d3b75
William Nettles <65319041+William-Nettles@users.noreply.github.com>
2024-05-12 15:17:23 -0400

    Frontend react to review (#18)
    * Add react-icons
    
    * Add skeleton for ReactToReview component
    
    * Reflect the changes in BaseLayout branch
    
    * Added react to review component
    
    * Made user context consistent with previous change
    
    ---------
    
    Co-authored-by: Neil C <74469287+sbacky@users.noreply.github.com>
    Co-authored-by: Mati Nemera <62734465+Kemosabe1900@users.noreply.github.com>
    Co-authored-by: kemosabe1900 <mati319@revature.net>
    Co-authored-by: MangerM <135554375+MangerM@users.noreply.github.com>
    Co-authored-by: Neil Crum <nhc.crum@outlook.com>

--- backend/p2Backend/src/main/java/com/revature/services/UserService.java
+++ backend/p2Backend/src/main/java/com/revature/services/UserService.java
@@ -1,8 +1,15 @@
 package com.revature.services;
 
 import com.revature.daos.UserDAO;
+import com.revature.models.User;
+
+import com.revature.models.dtos.OutgoingUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,5 +19,27 @@ public class UserService {
     @Autowired
     public UserService(UserDAO userDAO) {
         this.userDAO = userDAO;
+    }
+
+    public Optional<OutgoingUserDTO> findUserByUsername(String username){
+
+        Optional<User> user = userDAO.findByUsername(username);
+
+        if(user.isPresent()){
+            User u = user.get();
+            OutgoingUserDTO uout = new OutgoingUserDTO(
+                    u.getId(),
+                    u.getFirstName(),
+                    u.getLastName(),
+                    u.getUsername(),
+                    u.getRole(),
+                    u.getEmail(),
+                    u.getTimestamp()
+            );
+            return Optional.of(uout);
+        } else {
+            return Optional.empty();
+        }
+
     }
 }
