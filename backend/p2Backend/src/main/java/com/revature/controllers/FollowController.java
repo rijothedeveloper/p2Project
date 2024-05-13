1c79333eabb054b3c25863fc7e497343ee70993a
William Nettles <willnettles@gmail.com>
2024-05-12 22:57:38 -0400

    Added a few comments

--- backend/p2Backend/src/main/java/com/revature/controllers/FollowController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/FollowController.java
@@ -18,14 +18,14 @@ public class FollowController {
     public FollowController(FollowService followService) {
         this.followService = followService;
     }
-
+    //follow a user
     @PostMapping("/new_follow/{targetUserId}")
     public ResponseEntity<Object> followUser(@PathVariable int targetUserId, HttpSession session) {
-
+        //if user is not logged in, return 401, will update for JWT tokens later
         if ((int) session.getAttribute("userId") == 0) {
             return ResponseEntity.status(401).body("You must be logged in to follow a user.");
         }
-
+        //try to follow user, return 500 if error
         try {
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(followService.followUser(targetUserId, (int) session.getAttribute("userId")));
@@ -33,14 +33,14 @@ public class FollowController {
             return ResponseEntity.status(500).body(e.getMessage());
         }
     }
-
+    //unfollow a user
     @DeleteMapping("/unfollow/{targetUserId}")
     public ResponseEntity<Object> unfollowUser(@PathVariable int targetUserId, HttpSession session) {
-
+        //if user is not logged in, return 401, will update for JWT tokens later
         if ((int) session.getAttribute("userId") == 0) {
             return ResponseEntity.status(401).body("You must be logged in to unfollow a user.");
         }
-
+        //try to unfollow user, return 500 if error
         try {
             followService.unfollowUser(targetUserId, (int) session.getAttribute("userId"));
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
