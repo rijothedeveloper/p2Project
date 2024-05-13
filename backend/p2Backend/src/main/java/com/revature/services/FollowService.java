0c68e97806c1ad9f3d3528151a5e55d005c5d0e3
William Nettles <willnettles@gmail.com>
2024-05-12 22:57:38 -0400

    Added a few comments

--- backend/p2Backend/src/main/java/com/revature/services/FollowService.java
+++ backend/p2Backend/src/main/java/com/revature/services/FollowService.java
@@ -22,27 +22,27 @@ public class FollowService {
         this.userDAO = userDAO;
         this.followDAO = followDAO;
     }
-
+    // Follow a user
     public Follow followUser(int targetUserId, int userId) {
-
+        // Check if the user is trying to follow themselves
         if (userId == targetUserId) {
             throw new RuntimeException("You cannot follow yourself.");
         }
-
+        // Check if the user and target user exist
         Optional<User> user = userDAO.findById(userId);
         Optional<User> targetUser = userDAO.findById(targetUserId);
 
         if (user.isEmpty() || targetUser.isEmpty()) {
             throw new RuntimeException("User not found.");
         }
-
+        // Create a new follow object
         FollowKey key = new FollowKey(user.get(), targetUser.get());
         Follow follow = new Follow(key);
         return followDAO.save(follow);
     }
-
+    // Unfollow a user
     public void unfollowUser(int targetUserId, int userId) {
-
+            // Check if the user and target user exist
             Optional<User> user = userDAO.findById(userId);
             Optional<User> targetUser = userDAO.findById(targetUserId);
 
