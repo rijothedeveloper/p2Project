c1d57381de88018e450622f3ec7922debdf265b2
MangerM <mangerml1891@gmail.com>
2024-05-16 11:53:41 -0400

    Started view replies to review process

---
+++ frontend/src/Components/Replies/Reply.tsx
@@ -0,0 +1,11 @@
+export const Reply: React.FC = (reply:any) => {
+
+    //Reply needs reply information to be passed to it.  Once the information is received it will display the information within an accordian collection.
+    //Reply should have 2 elements the reply ID and the body.  Display the body for each reply since the reply ID isn't necessary.
+
+    return(
+        <div>
+
+        </div>
+    )
+}
--- frontend/src/Components/Review/ReviewModal.tsx
+++ frontend/src/Components/Review/ReviewModal.tsx
@@ -1,4 +1,17 @@
+import axios from "axios"
+import { useState } from "react"
+import { Reply } from "../Replies/Reply"
+
 export const ReviewModal: React.FC = () => {
+
+    const [replyCollection, setReplyCollection] = useState([])
+
+    //When the review loads then we need to fetch all replies for the current review.  Then we can properly map the replies to the reply component.
+    const fetchReplies = async () => {
+        //we request the list of replies for the current review at the specified endpoint
+        const response = axios.get("HTMLADDRESSNEEDSFIXING", {withCredentials:true})
+        .then((response) => {setReplyCollection(response.data)})
+    }
 
     return (
         <div>
@@ -7,6 +20,7 @@ export const ReviewModal: React.FC = () => {
              * the view of wherever we were when the review was deleted.  Without that information the view displaying the review will still show and might confuse users.
              * Alternatively we could close the review modal when the delete button is pressed (we will be opening up a new modal) and then simply refresh the content.
              */}
+             {replyCollection.map((reply:any) => {return(<Reply {...reply} key={reply.replyID}/>)})}
         </div>
     )
 }
