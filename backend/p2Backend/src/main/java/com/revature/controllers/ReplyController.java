f54ced298f7e564bc04e9b57cc2e5c94758bf7c0
William Nettles <willnettles@gmail.com>
2024-05-16 17:46:15 -0400

    created backend for reply-to-review

--- backend/p2Backend/src/main/java/com/revature/controllers/ReplyController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/ReplyController.java
@@ -1,19 +1,40 @@
 package com.revature.controllers;
 
+import com.revature.models.Reply;
+import com.revature.models.dtos.ReplyDTO;
 import com.revature.services.ReplyService;
+import com.revature.utils.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/replies")
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class ReplyController {
     ReplyService replyService;
+    JwtTokenUtil jwtUtil;
 
     @Autowired
-    public ReplyController(ReplyService replyService) {
+    public ReplyController(ReplyService replyService, JwtTokenUtil jwtUtil) {
         this.replyService = replyService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/{reviewId}")
+    public ResponseEntity<Object> addReply(@PathVariable int reviewId, @RequestBody ReplyDTO reply, @RequestHeader("Authorization") String token) {
+        String jwt = token.substring(7);
+        int userId = jwtUtil.extractUserId(jwt);
+        if (userId == 0) {
+            throw new RuntimeException("You must be logged in to reply to a review");
+        }
+
+        Reply newReply;
+        try {
+            newReply = replyService.addReply(reviewId, userId, reply);
+            return ResponseEntity.status(201).body(newReply);
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
     }
 }
--- backend/p2Backend/src/main/java/com/revature/services/ReplyService.java
+++ backend/p2Backend/src/main/java/com/revature/services/ReplyService.java
@@ -2,14 +2,32 @@ package com.revature.services;
 
 
 import com.revature.daos.ReplyDAO;
+import com.revature.daos.ReviewDAO;
+import com.revature.daos.UserDAO;
+import com.revature.models.Reply;
+import com.revature.models.dtos.ReplyDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReplyService {
 
     ReplyDAO replyDAO;
+    UserDAO userDAO;
+    ReviewDAO reviewDAO;
 
-    public ReplyService(ReplyDAO replyDAO) {
+    @Autowired
+    public ReplyService(ReplyDAO replyDAO, UserDAO userDAO, ReviewDAO reviewDAO) {
         this.replyDAO = replyDAO;
+        this.userDAO = userDAO;
+        this.reviewDAO = reviewDAO;
+    }
+
+    public Reply addReply(int reviewId, int userId, ReplyDTO reply) {
+        Reply newReply = new Reply();
+        newReply.setBody(reply.getBody());
+        newReply.setUser(userDAO.findById(userId).orElseThrow(() -> new IllegalArgumentException("No user found for ID: " + userId)));
+        newReply.setReview(reviewDAO.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("No review found for ID: " + reviewId)));
+        return replyDAO.save(newReply);
     }
 }
