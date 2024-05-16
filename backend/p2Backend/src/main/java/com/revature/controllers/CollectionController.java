5876388893437dbdd5a96dd9047caf1c715932d3
William Nettles <willnettles@gmail.com>
2024-05-16 13:59:04 -0400

    Added jwt

--- backend/p2Backend/src/main/java/com/revature/controllers/CollectionController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/CollectionController.java
@@ -3,8 +3,10 @@ package com.revature.controllers;
 import com.revature.models.Collection;
 import com.revature.models.Item;
 import com.revature.services.CollectionService;
+import com.revature.utils.JwtTokenUtil;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.revature.models.dtos.AddItemToCollectionDTO;
@@ -18,26 +20,34 @@ import java.util.List;
 @CrossOrigin // (origins = "http://localhost:3000", allowCredentials = "true")
 public class CollectionController {
 
-    CollectionService collectionService;
+    private CollectionService collectionService;
+    private JwtTokenUtil jwtUtil;
 
     @Autowired
-    public CollectionController(CollectionService collectionService) {
+    public CollectionController(CollectionService collectionService, JwtTokenUtil jwtUtil) {
         this.collectionService = collectionService;
+        this.jwtUtil = jwtUtil;
     }
 
     //get all items in the user's collection
     @GetMapping("/my_collection")
-    public ResponseEntity<List<Item>> getCollection(HttpSession session) {
-
-        int userId = 1;
-        //int userId = (int) session.getAttribute(("userId"));
+    public ResponseEntity<?> getCollection(@RequestHeader("Authorization") String token) {
+        String jwt = token.substring(7);
+        int userId = jwtUtil.extractUserId(jwt);
 
         //if the user is logged in, call userService.getCollection(), otherwise, return an empty list
-        return userId == 0? ResponseEntity.ok(new ArrayList<Item>()) : ResponseEntity.ok(collectionService.getCollection(userId));
+        return userId == 0 ? ResponseEntity.status(HttpStatus.FORBIDDEN).body("You must be logged in to view your collection")
+                : ResponseEntity.ok(collectionService.getCollection(userId));
     }
 
     @PostMapping
-    public ResponseEntity<String> addItemToCollection(@RequestBody AddItemToCollectionDTO addItemToCollectionDTO) {
+    public ResponseEntity<String> addItemToCollection(@RequestBody AddItemToCollectionDTO addItemToCollectionDTO, @RequestHeader("Authorization") String token) {
+
+        String jwt = token.substring(7);
+        int userId = jwtUtil.extractUserId(jwt);
+        if (userId != addItemToCollectionDTO.getUserId()) {
+            return ResponseEntity.status(401).body("You must be logged in to add an item to your collection");
+        }
 
         try {
             Collection collection = collectionService.addItemToCollection(addItemToCollectionDTO);
