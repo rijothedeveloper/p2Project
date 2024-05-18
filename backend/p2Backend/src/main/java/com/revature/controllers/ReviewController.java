01062f5137d79d076b75f9d8d52cb5a4e9d89e28
Viktor Pisarenko <Viktorpisarenko123@gmail.com>
2024-05-18 13:21:25 -0700

    merge conflicts handled dev <- findUsername

--- backend/p2Backend/src/main/java/com/revature/controllers/ReviewController.java
+++ backend/p2Backend/src/main/java/com/revature/controllers/ReviewController.java
@@ -55,20 +55,4 @@ public class ReviewController {
         }
     }
 
-
-
-
-
-
-    //This method will return a List of outbound review DTOs that all belong to a userId
-    @GetMapping("/{userId}")
-    public ResponseEntity<?> getAllRevByUserId(@PathVariable int userId) {
-        //TODO: Login security checks
-
-        try {
-            return ResponseEntity.ok(reviewService.getAllRevByUserId(userId));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
 }
