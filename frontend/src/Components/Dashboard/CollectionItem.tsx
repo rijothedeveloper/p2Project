c2e156a2c90a24be779c144085467da345f14f5f
w314 <piroska.werner@gmail.com>
2024-05-18 14:48:54 -0500

    Added View item details functionality

--- frontend/src/Components/Dashboard/CollectionItem.tsx
+++ frontend/src/Components/Dashboard/CollectionItem.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { Button, Card, Modal }from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../Contexts/UserContext";
 import { deleteItem } from "../../FrontendAPI/api";
@@ -8,6 +7,7 @@ import { baseURL } from "../../FrontendAPI/api";
 import { ItemInterface } from "../../Interfaces/ItemInterface"
 import { UserInterface } from "../../Interfaces/UserInterface";
 import { Login } from "../Login/Login";
+import { Link } from "react-router-dom";
 
 
 
@@ -20,6 +20,8 @@ const CollectionItem: React.FC<{
     item: ItemInterface,
     handleDeleteItem: (itemId: number) => void
 }> = ({ item, handleDeleteItem }) => {
+
+    const [ showItemDetails, setShowItemDetails ] = React.useState(false)
     
     const navigate = useNavigate();
 
@@ -30,15 +32,10 @@ const CollectionItem: React.FC<{
     // get currentUser used to deteremine wether to display delete item button or not
     const { currentUser } = React.useContext(UserContext)
     const { jwt } = currentUser as  UserInterface
-    const userRole = currentUser?.role == "USER" ? "user" : "admin"
+    const userRole: string = currentUser?.role == "USER" ? "user" : "admin"
 
 
-    // navigate to itemDetails if view details button is clicked
-    const handleClickViewDetailsButton = (event: React.MouseEvent<HTMLButtonElement>) => {
-        // TODO uncomment and test view items
-        // navigate to itemDetails
-        // navigate(`/${baseURL}/items/${id}`)
-    }
+
 
 
     // delet item from collection if delete button is clicked
@@ -49,6 +46,16 @@ const CollectionItem: React.FC<{
         handleDeleteItem(item.id as number)
     }
 
+    const handleClickViewDetailsButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+        // TODO uncomment and test view items
+        // navigate to itemDetails
+        // navigate(`/${baseURL}/items/${id}`)
+        setShowItemDetails(true)
+    }
+
+    const handleItemDetailsModalClose = () => {
+        setShowItemDetails(false)
+    }
 
     // function to determine color of rating
     const ratingColor = () => {
@@ -58,9 +65,10 @@ const CollectionItem: React.FC<{
       return "text-danger"
     }
 
-
+    
     return currentUser 
     ?  (
+        <>
         <Card style={{ width: '14rem' }} className="m-1">
             <Card.Img variant="top" src={ image } className="mt-2"/>
             <Card.Body>
@@ -79,15 +87,69 @@ const CollectionItem: React.FC<{
                     className = "mr-6"
                     onClick={handleClickViewDetailsButton}
                 >View Details</Button>
+                 {/* <Button 
+                    variant="outline-primary"
+                    size="sm"
+                    className = "mr-6"
+                >View Details</Button> */}
                 {`     `}
                 <Button
                     variant="outline-danger"
                     size="sm"    
                     onClick = { handleDeleteItemButtonClick }
-                    hidden = { userRole == "user" }
+                    hidden = { userRole === "user" }
                 >Delete</Button>
             </Card.Body>
         </Card>
+        
+      <Modal show={showItemDetails} onHide={handleItemDetailsModalClose}>
+       <Modal.Header closeButton>
+         <Modal.Title>{item.name}</Modal.Title>
+       </Modal.Header>
+       <Modal.Body>
+       <Card style={{ width: '18rem' }} className="m-1">
+            <Card.Img variant="top" src={ image } className="mt-2"/>
+            <Card.Body>
+                <Card.Text>
+                    <div>
+                        Category: {item.category}
+                    </div>
+                    {/* <div>
+                        Producer: {item.producerId}
+                    </div> */}
+                    <div>
+                        Rating: 
+                            <span id="rating" className={ratingColor()}>
+                                {/* TODO find beter way for spacing  */}
+                                {`  ${rating}`}
+                            </span>
+                    </div>
+                    <div>
+                        Description: {item.description}
+                    </div>
+
+                </Card.Text>
+            </Card.Body>
+        </Card>
+       </Modal.Body>
+       <Modal.Footer>
+
+         <Button
+                    variant="outline-danger"
+                    size="sm"    
+                    onClick = { handleDeleteItemButtonClick }
+                    hidden = { userRole == "user" }
+            >Delete</Button>
+         {/* <Button variant="primary" onClick={handleItemDetailsModalClose}>
+           Save Changes
+         </Button> */}
+
+        <Button variant="secondary" onClick={handleItemDetailsModalClose}>
+           Close
+         </Button>
+       </Modal.Footer>
+     </Modal>
+        </>
     )
     : <Login />
 }
