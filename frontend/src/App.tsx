aaad759878dbb7c535e24ccb54e4ed9114ac115a
w314 <piroska.werner@gmail.com>
2024-05-17 22:46:49 -0500

    Added Dashboard Component'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import { UserInterface } from './Interfaces/UserInterface';
 import { Layout } from './Components/Layout/Layout';
 import { AllUsers } from './Components/Users/AllUsers';
 import { UserProvider } from './Contexts/UserProvider';
+import { Dashboard } from './Components/Dashboard/Dashboard';
 
 function App() {
 
@@ -27,7 +28,7 @@ function App() {
                         <Route path="/allusers" element={<AllUsers />} />
                         <Route path="/" element={<Layout />}>
                             {/* Include all routes with a navbar below */}
-                            {/*<Route path="dashboard" element={} />*/}
+                            <Route path="dashboard" element={< Dashboard />} />
                             <Route path="user/:userId" element={<UserDetails />} />
                         </Route>
                     </Routes>
---
+++ frontend/src/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,76 @@
+import { useContext, useState } from "react";
+import { Container, Form } from "react-bootstrap"
+import { UserContext } from "../../Contexts/UserContext";
+import { useNavigate } from "react-router-dom";
+// import Collection from "./Collection";
+// import ReviewList from "./ReviewList";
+
+export const Dashboard: React.FC = () => {
+
+    // TODO: Create the actual dashboard. This is skeleton code for the dashboard component. It should display the current users collection of items, reviews and maybe their information.
+
+    // // Navigation to navigate to different pages
+    // const navigate = useNavigate();
+    // // Get currentUser from user context
+    // const { currentUser } = useContext(UserContext);
+    // // Check if a user is logged in
+    // if (!currentUser) {
+    //     navigate("/");
+    // }
+
+    // Current view state - defaults to myCollection
+    const [currentView, setCurrentView] = useState("myCollection");
+    // My collection state - defaults to an empty array
+    const [myCollection, setMyCollection] = useState([]);
+    // My reviews state - defaults to an empty array
+    const [myReviews, setMyReviews] = useState([]);
+
+    /**
+     * Handles the select view on change to change the current view in the dashboard
+     * @param input - select input value
+     */
+    const handleSelectViewOnChange = (input: React.ChangeEvent<HTMLSelectElement>) => {
+        const view: string = input.target.value;
+        switch (view) {
+            case "myCollection":
+                // TODO: add function to get collection items for logged in users
+                setCurrentView("myCollection");
+                break;
+            case "myReviews":
+                // TODO: add function to get review items for logged in users
+                setCurrentView("myReviews");
+                break;
+            default:
+                // This should never run, but just in case
+                alert("Invalid view type!");
+                break;
+        }
+    };
+
+    return (
+        <>
+            <Container id="profile-container">
+                {/* Can add logged in user information here */}
+            </Container>
+            <Container id="select-view-container">
+                <Form id="select-view-form">
+                    <Form.FloatingLabel label="Select View" controlId="dashboard-select-view" onChange={handleSelectViewOnChange}>
+                        <Form.Select id="selectMyView" defaultValue="myCollection">
+                            <option value="myCollection">My Collection</option>
+                            <option value="myReviews">My Reviews</option>
+                        </Form.Select>
+                    </Form.FloatingLabel>
+                </Form>
+            </Container>
+            <Container id="collection-review-container">
+                {/* Conditionally render my collection or my reviews based on currentView */}
+                { currentView === "myCollection"
+                // ? (<Collection />)
+                // : <ReviewList 
+                ? <h2>CollectionList</h2>
+                : <h2>ReviewList</h2>
+                }
+            </Container>
+        </>
+    )
+}
