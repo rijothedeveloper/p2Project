25199606d86b6bb4175c8b52800bfc586eefd9e4
Neil Crum <nhc.crum@outlook.com>
2024-05-19 02:14:32 -0700

    Update navbar to show active link

--- frontend/src/Components/NavbarComp/NavbarComp.tsx
+++ frontend/src/Components/NavbarComp/NavbarComp.tsx
@@ -1,7 +1,25 @@
 import { Button, Container, Nav, Navbar } from "react-bootstrap"
 import { DeleteMyAccount } from "../Users/DeleteMyAccount";
+import { useEffect } from "react";
 
 export const NavbarComp: React.FC = () => {
+
+    const upateNavbar = () => {
+        const currentURL = new URL(window.location.href);
+        const path = currentURL.pathname;
+        console.log(path);
+        const navbarItems = document.querySelectorAll(".nav-link");
+        navbarItems.forEach((item) => {
+            //item.classList.remove("active");
+            if (item.getAttribute("href") == path) {
+                item.classList.add("active");
+            }
+        });
+    };
+
+    useEffect(() => {
+        upateNavbar();
+    }, []);
 
     const logout = () => {
         // TODO: implement logout function
@@ -14,9 +32,9 @@ export const NavbarComp: React.FC = () => {
                 <Navbar.Toggle aria-controls="social-review-navbar-nav" />
                 <Navbar.Collapse id="social-review-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link>Dashboard</Nav.Link>
-                        <Nav.Link>Collections</Nav.Link>
-                        <Nav.Link>Users</Nav.Link>
+                        <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                        <Nav.Link href="">Collections</Nav.Link>
+                        <Nav.Link href="/allusers">Users</Nav.Link>
                     </Nav>
                     <Button type="button" variant="danger" onClick={logout}>Logout</Button>
                     <DeleteMyAccount />
