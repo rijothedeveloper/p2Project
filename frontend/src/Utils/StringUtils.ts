338e31b14ed017091d008c114eeb1c6a90f93ae2
Neil Crum <nhc.crum@outlook.com>
2024-05-19 16:44:00 -0700

    Add a capitalize string util function

--- frontend/src/Utils/StringUtils.ts
+++ frontend/src/Utils/StringUtils.ts
@@ -12,3 +12,18 @@ export const truncateText = (text: string, maxLength: number, useElipsis: boolea
     const truncatedText = text.substring(0, maxLength - (useElipsis ? 3 : 0));
     return useElipsis ? `${truncateText}...` : truncatedText;
 }
+
+/**
+ * Capitalizes the given text and removes any leading and trailing whitespace.
+ * @param text - the text to capitalize
+ * @returns return the capitalized text with no leading or trailing whitespace.
+ */
+export const capitalize = (text: string) => {
+    if (!text.trim()) {
+        return text;
+    }
+    const trimmedText = text.trim();
+    const firstLetter = trimmedText.charAt(0).toUpperCase();
+    const restOfText = trimmedText.slice(1).toLowerCase()
+    return firstLetter + restOfText;
+}
