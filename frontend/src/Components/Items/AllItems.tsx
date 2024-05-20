5e26356f77837e13a0cc80305cc7b000fe6f57aa
Neil Crum <nhc.crum@outlook.com>
2024-05-20 00:43:43 -0700

    Add switches to set/view current grid view and add name filter component

--- frontend/src/Components/Items/AllItems.tsx
+++ frontend/src/Components/Items/AllItems.tsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { ItemInterface } from "../../Interfaces/ItemInterface";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import { UserContext } from "../../Contexts/UserContext";
 import { getAllItems, getItemsByCategory } from "../../FrontendAPI/api";
 import { ItemsByCategory } from "./ItemsByCategory";
 import { ItemColumns } from "./ItemColumns";
+import { ItemsByName } from "./ItemsByName";
 
 export const AllItems: React.FC = () => {
 
@@ -13,7 +14,10 @@ export const AllItems: React.FC = () => {
     const [category, setCategory] = useState('');
     const [categoryItems, setCategoryItems] = useState<ItemInterface[]>([]);
     const [categoryOptions, setCategoryOptions] = useState<string[]>([]);
+    const [nameFilter, setNameFilter] = useState<string>("");
+    const [nameItems, setNameItems] = useState<ItemInterface[]>([]);
     const [items, setItems] = useState<ItemInterface[]>([]);
+    const [view, setView] = useState<string>("category");
 
     // Fetch all items
     const fetchItems = async () => {
@@ -24,6 +28,14 @@ export const AllItems: React.FC = () => {
         } else {
             console.log(response.message);
             setItems(response.data);
+        }
+    };
+    // Handle switch change
+    const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value === view){
+            setView(view === "category" ? "name": "category");
+        } else {
+            setView(event.target.value);
         }
     };
     
@@ -42,19 +54,27 @@ export const AllItems: React.FC = () => {
     }, [items]);
     // Filter list of items for items with category
     useEffect(() => {
-        const itemsWithCategory = items.filter((item) => {
-            if (item.category === category) {
-                return item;
-            }
-        });
+        const itemsWithCategory = items.filter(item => item.category === category);
         setCategoryItems(category === "" ? items : itemsWithCategory);
     }, [category]);
+    // Filter list of items for items with name
+    useEffect(() => {
+        const itemsWithName = items.filter(item => item.name.toLowerCase().indexOf(nameFilter.toLowerCase()) > -1);
+        setNameItems(itemsWithName);
+    }, [nameFilter])
 
     return (
         <Container>
+            <Container id="categorySwitch" className="ms-3">
+                <Form.Switch inline id="categoryFilter" name="selectView" label="Filter by Category" value="category" checked={view === "category"} onChange={handleSwitchChange} />
+            </Container>
             <ItemsByCategory setCategory={setCategory} categoryOptions={categoryOptions} />
-            <Container className="mt-3">
-                <ItemColumns items={categoryItems} />
+            <Container id="nameSwitch" className="mt-3 ms-3">
+                <Form.Switch inline id="nameFilter" name="selectView" label="Filter by Name" value="name" checked={view === "name"} onChange={handleSwitchChange} />
+            </Container>
+            <ItemsByName setNameFilter={setNameFilter} />
+            <Container id="itemGrid" className="mt-3">
+                <ItemColumns items={view === "category" ? categoryItems : nameItems} />
             </Container>
         </Container>
     )
