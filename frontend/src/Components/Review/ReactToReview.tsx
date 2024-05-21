27f76ff1a69a086e67c8e321a2a73b4b210606d1
Neil Crum <nhc.crum@outlook.com>
2024-05-21 04:15:50 -0700

    Add Score methods and refactor react to review

--- frontend/src/Components/Review/ReactToReview.tsx
+++ frontend/src/Components/Review/ReactToReview.tsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import { BsHandThumbsDown, BsHandThumbsDownFill, BsHandThumbsUp, BsHandThumbsUpFill } from "react-icons/bs";
 import { UserContext } from "../../Contexts/UserContext";
 import { ReviewInterface } from "../../Interfaces/ReviewInterface";
-import { addScore, getUserScore } from "../../FrontendAPI/api";
+import { deleteVote, getUserVote, newVote, updateVote } from "../../FrontendAPI/api";
 
 export const ReactToReview: React.FC<ReviewInterface> = (review:ReviewInterface) => {
 
@@ -11,13 +11,23 @@ export const ReactToReview: React.FC<ReviewInterface> = (review:ReviewInterface)
     const [userVoteSignal, setUserVoteSignal] = useState<number>(0);
     const [votes, setVotes] = useState<number>(review.score || 0);
 
-    const vote = addScore(currentUser?.jwt as string, review, userVoteSignal).then((response) => {
-        if (response) {
-            console.log(response);
-        } else {
-            console.error(response);
+    useEffect(() => {
+        if (currentUser) {
+            const fetchVote = async () => {
+                if (currentUser?.jwt && review.id) {
+                    const response = await getUserVote(currentUser.jwt, review.id);
+                    if (!response.status) {
+                        console.error(response.message);
+                        alert(response.message);
+                    } else {
+                        console.log(response.message);
+                        setUserVoteSignal(response.data);
+                    }
+                }
+            }
+            fetchVote();
         }
-    });
+    }, [currentUser, review.id]);
 
     const handleVote = async (newSignal: number) => {
         // reset vote to 0
@@ -30,70 +40,31 @@ export const ReactToReview: React.FC<ReviewInterface> = (review:ReviewInterface)
         setUserVoteSignal(newSignal);
         setVotes(votes + voteChange);
 
-
-    }
-
-    
-    const handleUpvote = () => {
-        // handle changing user vote to neutral
-        if (userVoteSignal > 0) {
-            setVotes(votes - 1);
-            setUserVoteSignal(0);
-            return;
+        let response;
+        if (newSignal === 0) {
+            response = await deleteVote(currentUser?.jwt as string, review.id as number);
+        } else if (userVoteSignal === 0) {
+            response = await newVote(currentUser?.jwt as string, review.id as number, newSignal);
+        } else {
+            response = await updateVote(currentUser?.jwt as string, review.id as number, newSignal);
         }
-        // Handle changing user vote from downvote to upvote
-        if (userVoteSignal < 0) {
-            setVotes(votes + 1);
-        }
-        setVotes(votes + 1);
-        setUserVoteSignal(1);
-        addScore(currentUser?.jwt as string, review, 1).then((response) => {
-            if (response) {
-                console.log(response);
-            } else {
-                console.error(response);
-            }
-        });
 
+        if (!response.status) {
+            console.error(response.message);
+            alert(response.message);
+        } else {
+            console.log(response.message);
+        }
     };
-    const handleDownvote = () => {
-        // handle changing user vote to neutral
-        if (userVoteSignal < 0) {
-            setVotes(votes + 1);
-            setUserVoteSignal(0);
-            return;
-        }
-        // handle changing user vote from upvote to downvote
-        if (userVoteSignal > 0) {
-            setVotes(votes - 1);
-        }
-        setVotes(votes - 1);
-        setUserVoteSignal(-1)
-        if (userVoteSignal === 0) {
-            addScore(currentUser?.jwt as string, review, -1).then((response) => {
-                if (response) {
-                    console.log(response);
-                } else {
-                    console.error(response);
-                }
-            });
-        } else {}
-    };
-    
-
-    useEffect(() => {
-        getUserScore(currentUser?.jwt as string, review.id as number).then((response) => {
-            setUserVoteSignal(response);
-        });
-    }, [currentUser, review.id]);
 
     return (
         <Container>
             <h5>Rate Review</h5>
             <Container className="d-flex">
-                {userVoteSignal > 0 ? <BsHandThumbsUpFill className="text-success" onClick={handleUpvote}/> : <BsHandThumbsUp onClick={handleUpvote}/>}
+                {userVoteSignal > 0 ?
+                <BsHandThumbsUpFill className="text-success" onClick={()=> handleVote(1)}/> : <BsHandThumbsUp onClick={() => handleVote(1)}/>}
                 <p>{votes}</p>
-                {userVoteSignal < 0 ? <BsHandThumbsDownFill className="text-danger" onClick={handleDownvote}/> : <BsHandThumbsDown onClick={handleDownvote}/>}
+                {userVoteSignal < 0 ? <BsHandThumbsDownFill className="text-danger" onClick={() => handleVote(-1)}/> : <BsHandThumbsDown onClick={() => handleVote(-1)}/>}
             </Container>
         </Container>
     )
