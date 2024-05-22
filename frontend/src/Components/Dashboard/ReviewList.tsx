de69ef22e28de122ca235c9ac87bd412018ef95f
w314 <piroska.werner@gmail.com>
2024-05-21 20:05:36 -0500

    Fixed key warning in Reveiws

--- frontend/src/Components/Dashboard/ReviewList.tsx
+++ frontend/src/Components/Dashboard/ReviewList.tsx
@@ -34,11 +34,10 @@ const ReviewList: React.FC<{
                 {reviews && reviews
                     // filter items based on nameFilter
                     .map(itemReview => {
-                    {console.log(`ITEM REVIEW ID: ${JSON.stringify(itemReview.id)}`)}
+                    // {console.log(`ITEM REVIEW ID: ${JSON.stringify(itemReview.id)}`)}
                     return (
-                        <Col>
+                        <Col key = {itemReview.id as number}>
                             <Review 
-                                key = {itemReview.id as number}
                                 itemReview = { itemReview } 
                                 handleEditReview = { handleEditReview }
                                 handleDeleteReview={handleDeleteReview}
